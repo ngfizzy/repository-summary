703e2b3f5be8fb6269b0b82476ef6532025e4c39
Olufisayo Bamidele <fisiwizy@gmail.com>
2020-11-26 03:47:06 +0100

    implement full functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import SearchSection from './SearchSection/SearchSection'
 import './App.css';
 function App() {
+
+  const [averageCommits, setAverageCommits] = useState(0);
+  const [repo, setRepo] = useState();
+  const [last3Commits, setLast3Commits] = useState([]);
+
+  async function getRepoDetails(selectedRepo) {
+    setRepo(selectedRepo)
+
+    const { all: weeklyCommits} = await fetch(`https://api.github.com/repos/${selectedRepo.owner.login}/${selectedRepo.name}/stats/participation`)
+      .then(res => res.json());
+      const commits = await fetch(`https://api.github.com/repos/${selectedRepo.owner.login}/${selectedRepo.name}/commits`)
+      .then(res => res.json())
+ 
+    const aveWeeklyCommits = (weeklyCommits.reduce((prev, curr) => (prev + curr)) / weeklyCommits.length);
+    const last3Coms =  commits.splice(0, 3).map(({commit: { message }}) =>  {
+      let titleEnd = message.indexOf('\n');
+      titleEnd = titleEnd > -1 ? titleEnd : message.length;
+
+      return message.substring(0, titleEnd)
+    }); 
+
+    setAverageCommits(aveWeeklyCommits);
+    setLast3Commits(last3Coms);
+  }
+
   return (
     <div className="App">
-      <SearchSection />
+      <SearchSection getRepoDetails={getRepoDetails}/>
+
+      <section style={{padding: '1rem'}}>
+        <h3 style={{textAlign: 'center'}}>Repo Summary</h3> 
+        {repo? (<div>
+          <div>Average Commits Per Week: <span>{Math.floor(averageCommits)}</span></div>
+        </div>): null }
+        {last3Commits?.length ?<h4>Last 3 commits</h4>: null}
+        <ul>
+          {last3Commits.map((message, index) =><li key={index}>{message}</li> )}
+        </ul>
+      </section>
     </div>
   );
 }
--- src/SearchSection/SearchSection.js
+++ src/SearchSection/SearchSection.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './SearchSection.css'
 
-export default function SearchSection() {
+export default function SearchSection({ getRepoDetails }) {
 
-    function searchRepo(event) {
+    const [submitted, setSubmitted ] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [repos, setRepos] = useState([]);
+
+    async function findRepo() {
+       const result = await fetch(`https://api.github.com/search/repositories?q=${searchTerm}`)
+        .then(res => res.json());
+
+        setRepos(result.items.splice(0, 10));
+        
+    }
+
+    useEffect(() => {
+        if(submitted) {
+            findRepo();
+        } else {
+            setRepos(() => [])
+        }
+
+    }, [submitted])
+
+    function perfSubmit(event) {
         event.preventDefault();
-        console.log('repo>>>>>>>>>>>>>>>>>>>> search', event)
+        setSubmitted(true);
     }
 
     return <section className="SearchSection Background-White"> 
-            <form onSubmit={e => searchRepo(e)}>
-                <input  placeholder="Search Repository"/>
+            <form onSubmit={e => perfSubmit(e)}>
+                <input
+                    value={searchTerm}
+                    placeholder="Search Repository"
+                    onChange={e => {setSearchTerm(e.target.value); setSubmitted(false)}}
+                />
                 <button  type="submit" className="Search">
                     Search
                 </button>
             </form>
 
             <div className="SearchResult">
-
+                <ul>
+                 {repos.map(repo => <li key={repo.id} onClick={() => getRepoDetails(repo)}>{repo.name}</li>)}
+                </ul>
             </div>
     </section>
 };
